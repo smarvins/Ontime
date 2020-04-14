ddd73053294ea21fbf6daf314fa921d4d4aea478
Shavolski-dev <stevewachiradev@gmail.com>
2020-04-14 19:44:22 -0400

    imported material and animation packages to the project

--- ontime/src/app/app.module.ts
+++ ontime/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* These are modules that will take charge on importing the material and animation packages */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
