b0af6ee7e56b5176f87236194f4e7efc2a528497
Shavolski-dev <stevewachiradev@gmail.com>
2020-04-15 00:15:12 -0400

    fixed bug on the export class NewsApiService

--- ontime/src/app/newsapi.service.ts
+++ ontime/src/app/newsapi.service.ts
@@ -9,12 +9,12 @@ export class NewsapiService {
 
   constructor( private http:HttpClient ) { }
   initSources(){
-    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+78607a355fd24d618fdf7e30ace4a375)
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key)
   }
   initArticles(){
-    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+78607a355fd24d618fdf7e30ace4a375);
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
   }
   getArticlesById(source: String){
-    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+78607a355fd24d618fdf7e30ace4a375);
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
   }
 }
