037f9bc31329f1462fa95aae144cff648482d4d5
Shavolski-dev <stevewachiradev@gmail.com>
2020-04-14 22:40:42 -0400

    generated an api key and attaching the relevant endpoints to be used

--- ontime/src/app/newsapi.service.ts
+++ ontime/src/app/newsapi.service.ts
@@ -5,5 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class NewsapiService {
 
-  constructor() { }
+  api_key = '78607a355fd24d618fdf7e30ace4a375';
+
+  constructor( private http:HttpClient ) { }
+  initSources(){
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+78607a355fd24d618fdf7e30ace4a375)
+  }
+  initArticles(){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+78607a355fd24d618fdf7e30ace4a375);
+  }
+  getArticlesById(source: String){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+78607a355fd24d618fdf7e30ace4a375);
+  }
 }
